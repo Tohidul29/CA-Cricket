d09270d2ea63dee9b90101c21bbaa39dca1979a9
Tohidul29 <tohidswfu@gmail.com>
2022-04-05 05:33:42 +0600

    graph chart finished

--- src/component/Dashboard/Dashboard.js
+++ src/component/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Dashboard.css';
-import { Bar, BarChart, Legend, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
     const [info, setInfo] = useState([]);
@@ -11,41 +11,55 @@ const Dashboard = () => {
     }, [])
     return (
         <div>
-            <h1>This is dashboard page</h1>
+            <h1 className='main-head'>Let's see our company's analysis bars</h1>
             <div className='graphs'>
-                {
-                    <LineChart width={500} height={300} data={info}>
-                        <Line dataKey={'investment'}></Line>
-                        <XAxis dataKey={'month'}></XAxis>
-                        <YAxis></YAxis>
-                        <Tooltip></Tooltip>
-                        <Legend></Legend>
-                    </LineChart>
-                }
-                {
-                    <BarChart width={500} height={300} data={info}>
-                        <Bar dataKey={'revenue'} fill="#00D84A"></Bar>
-                        <YAxis></YAxis>
-                        <XAxis dataKey={'month'}></XAxis>
-                        <Tooltip></Tooltip>
-                        <Legend></Legend>
-                    </BarChart>
-                }
-                {
-                    <PieChart width={400} height={400}>
-                        <Pie 
-                        dataKey={'sell'}
-                        isAnimationActive={true}
-                        data={info}
-                        cx='50%'
-                        cy='50%'
-                        outerRadius={150}
-                        fill='#00D84A'
-                        ></Pie>
-                        <Tooltip></Tooltip>
-                        <Legend></Legend>
-                    </PieChart>
-                }
+                <div>
+                    <h3>Investment LineChart</h3>
+                    {
+                        <LineChart width={500} height={300} data={info}>
+                            <Line dataKey={'investment'}></Line>
+                            <XAxis dataKey={'month'}></XAxis>
+                            <YAxis></YAxis>
+                            <Tooltip></Tooltip>
+                            <Legend></Legend>
+                        </LineChart>
+                    }
+                </div>
+                <div>
+                    <h3>Revenue BarChart</h3>
+                    {
+                        <BarChart width={500} height={300} data={info}>
+                            <Bar dataKey={'revenue'} fill="#00D84A"></Bar>
+                            <YAxis></YAxis>
+                            <XAxis dataKey={'month'}></XAxis>
+                            <Tooltip></Tooltip>
+                            <Legend></Legend>
+                        </BarChart>
+                    }
+                </div>
+                <div>
+                    <h3>Sell AreaChart</h3>
+                    {
+                        <AreaChart
+                            width={500}
+                            height={400}
+                            data={info}
+                            margin={{
+                                top: 10,
+                                right: 30,
+                                left: 0,
+                                bottom: 0,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="month" />
+                            <YAxis />
+                            <Tooltip />
+                            <Area type="monotone" dataKey="sell" stroke="#8884d8" fill="#8884d8" />
+                            <Legend></Legend>
+                        </AreaChart>
+                    }
+                </div>
             </div>
         </div>
     );
